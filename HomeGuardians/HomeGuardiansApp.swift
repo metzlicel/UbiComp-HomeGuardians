05115c7495d0842a022a247dff747b489d0e80e8
marlonca <marloncorona.a@gmail.com>
2026-03-15 19:41:55 -0700

    Initial Commit

---
+++ HomeGuardians/HomeGuardiansApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeGuardiansApp.swift
+//  HomeGuardians
+//
+//  Created by Marlon Corona Arango on 15/03/26.
+//
+
+import SwiftUI
+
+@main
+struct HomeGuardiansApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
